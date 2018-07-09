e55f9dcfb21cb9955ce20e10847ac3c595fc9c44
wangqiang <qiang.wang@seedeer.com>
2018-07-09 17:56:36 +0800

    swagger注释

--- SpringBoot-Web/src/main/java/com/web/model/ResultModel.java
+++ SpringBoot-Web/src/main/java/com/web/model/ResultModel.java
@@ -1,14 +1,22 @@
 package com.web.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @Description: 统一返回接口格式结果集
  * @Date: 2017-12-16 19:56
  */
+@ApiModel("响应结果实体类")
 public class ResultModel<T> {
+
+    @ApiModelProperty("响应码")
     private Integer code;
 
+    @ApiModelProperty("响应信息")
     private String msg;
 
+    @ApiModelProperty("响应结果")
     private T data;
 
     public ResultModel() {

932faff10865807c475f9224b982d03fd90b4417
Dragon-zg <wang.1991>
2018-07-14 10:20:34 +0800

    代码规范

--- SpringBoot-Common-Utils/src/main/java/com/common/utils/PropertiesUtil.java
+++ SpringBoot-Common-Utils/src/main/java/com/common/utils/PropertiesUtil.java
@@ -72,8 +72,8 @@ public class PropertiesUtil {
 		Enumeration en = props.propertyNames();
 		while (en.hasMoreElements()) {
 			String key = (String) en.nextElement();
-			String Property = props.getProperty(key);
-			map.put(key, Property);
+			String property = props.getProperty(key);
+			map.put(key, property);
 		}
 		return map;
 	}

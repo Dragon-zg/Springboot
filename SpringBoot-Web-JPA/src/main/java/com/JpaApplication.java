1ad8372fc31f39156ac1d3b70934d1f81b8fd158
Dragon-zg <wang.1991>
2018-07-15 14:44:35 +0800

    init

--- SpringBoot-Web-JPA/src/main/java/com/JpaApplication.java
+++ SpringBoot-Web-JPA/src/main/java/com/JpaApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JPAApplication {
+public class JpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JPAApplication.class, args);
+        SpringApplication.run(JpaApplication.class, args);
     }
 }

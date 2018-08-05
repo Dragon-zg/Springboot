fccec689179f0bb87c0ba3e022cf625f36dd6af1
Dragon-zg <wang.1991>
2018-08-05 12:14:02 +0800

    异常编码枚举类

--- SpringBoot-Web/src/main/java/com/web/enums/ExceptionCode.java
+++ SpringBoot-Web/src/main/java/com/web/enums/ExceptionCode.java
@@ -2,16 +2,30 @@ package com.web.enums;
 
 /**
  * @Description: 异常编码枚举类
+ * @Author Dragon-zg
  * @Date: 2017-12-16 20:34
  */
 public enum ExceptionCode {
+    /**
+     * 操作成功 200
+     */
     SUCCESS(200, "操作成功!"),
+    /**
+     * 未知错误 999999
+     */
     UNKOW_ERROR(999999, "未知错误!"),
-
-    //301 ~ 399 上传异常定义
+    /**
+     * 上传文件不存在 301
+     */
     UPLOAD_FILE_UNEXIST(301, "上传文件不存在!"),
-    UPLOAD_FORMAL_ERROR(301, "上传文件类型不正确!"),
-    UPLOAD_SIZE_ERROR(301, "上传文件大小超过限制!"),
+    /**
+     * 上传文件类型不正确 302
+     */
+    UPLOAD_FORMAL_ERROR(302, "上传文件类型不正确!"),
+    /**
+     * 上传文件大小超过限制 303
+     */
+    UPLOAD_SIZE_ERROR(303, "上传文件大小超过限制!"),
 
     NOT_EXIST_GIRL(1000,"不存在该女生!"),
     SMALL_GIRL(1001,"女生年纪过小,不允许查询!")

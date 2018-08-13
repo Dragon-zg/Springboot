cb9cc2cd0fc21f2e0b3c6e5e6d158cfa043c9b98
wangqiang <qiang.wang@seedeer.com>
2018-08-13 16:55:21 +0800

    MVC基础配置

---
+++ SpringBoot-Web/src/main/java/com/web/config/MvcConfig.java
@@ -0,0 +1,80 @@
+package com.web.config;
+
+import com.web.i18n.I18nMessageResource;
+import com.web.utils.Profiles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Locale;
+
+/**
+ * MVC基础配置
+ * @author Dragon-zg
+ * @date 2018/8/13 16:53
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    private final static Logger logger = LoggerFactory.getLogger(MvcConfig.class);
+
+    public MvcConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    private final Environment environment;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        if (environment.acceptsProfiles(Profiles.DEVELOPMENT, Profiles.PRODUCTION)) {
+        }
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        MessageSource messageSource = new I18nMessageResource();
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.CHINESE);
+        localeResolver.setCookieName("language");
+        return localeResolver;
+    }
+
+    @Profile({Profiles.DEVELOPMENT, Profiles.PRODUCTION})
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setEnvironment(environment);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST : ");
+        return filter;
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}

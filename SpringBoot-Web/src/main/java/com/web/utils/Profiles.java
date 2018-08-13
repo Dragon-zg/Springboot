f03699608f9537778a96bfb584bfd938c3d68ad3
wangqiang <qiang.wang@seedeer.com>
2018-08-13 16:47:27 +0800

    环境常量

---
+++ SpringBoot-Web/src/main/java/com/web/utils/Profiles.java
@@ -0,0 +1,17 @@
+package com.web.utils;
+
+/**
+ * 环境常量
+ * @Author Dragon-zg
+ * @Date 2018/8/13 16:05
+ **/
+public interface Profiles {
+    /**
+     * 测试环境
+     */
+    String DEVELOPMENT = "development";
+    /**
+     * 生产环境
+     */
+    String PRODUCTION = "production";
+}

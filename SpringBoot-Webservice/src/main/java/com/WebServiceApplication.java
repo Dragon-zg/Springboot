c0cde625b13b60f345a1bae0af89bdefe7f385ab
wangqiang <qiang.wang@seedeer.com>
2018-08-16 13:26:01 +0800

    init

--- SpringBoot-Webservice/src/main/java/com/WebServiceApplication.java
+++ SpringBoot-Webservice/src/main/java/com/WebServiceApplication.java
@@ -3,6 +3,12 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 可用apache-CXF或wsdl2java根据wsdl逆向生成相关代码
+ * https://www.cnblogs.com/ChrisMurphy/p/5224160.html
+ * @author Dragon-zg
+ * @date 2018/8/16 13:25
+ */
 @SpringBootApplication
 public class WebServiceApplication {
 

3bf5536c3de36ebab17c6d8be5fe35d35ddedae8
wangqiang <qiang.wang@seedeer.com>
2018-08-31 15:54:48 +0800

    init

--- SpringBoot-Web-JPA/src/main/java/com/JpaApplication.java
+++ SpringBoot-Web-JPA/src/main/java/com/JpaApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -10,6 +11,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
  * @date 2018/8/28 19:44
  */
 @SpringBootApplication
+@EntityScan(basePackages = {"com.jpa.entity"})
 @EnableSpringDataWebSupport
 @PropertySource("classpath:env/${spring.profiles.active}/application.properties")
 public class JpaApplication {

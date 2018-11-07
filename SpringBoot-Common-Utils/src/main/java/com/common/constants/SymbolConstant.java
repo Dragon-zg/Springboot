2fabe90c4b1736388f17519c6735e7248c2cf3dc
wangqiang <qiang.wang@seedeer.com>
2018-11-07 09:43:50 +0800

    符号常量类

---
+++ SpringBoot-Common-Utils/src/main/java/com/common/constants/SymbolConstant.java
@@ -0,0 +1,86 @@
+package com.common.constants;
+
+/**
+ * 符号常量类
+ *
+ * @author Dragon-zg
+ * @date 2018/11/7 9:39
+ */
+public interface SymbolConstant {
+
+    /**
+     * 空格
+     */
+    String SYMBOL_BLANK = " ";
+
+    /**
+     * 点
+     */
+    String SYMBOL_POINT = ".";
+
+    /**
+     * 冒号
+     */
+    String SYMBOL_COLON = ":";
+
+    /**
+     * 分号
+     */
+    String SYMBOL_SEMICOLON = ";";
+
+    /**
+     * 等于
+     */
+    String SYMBOL_EQUALS = "=";
+
+    /**
+     * 左括弧
+     */
+    String SYMBOL_BRACKET_LEFT = "(";
+
+    /**
+     * 右括弧
+     */
+    String SYMBOL_BRACKET_RIGHT = ")";
+
+    /**
+     * 单引号
+     */
+    String SYMBOL_QUOTE_SINGLE = "'";
+
+    /**
+     * 双引号
+     */
+    String SYMBOL_QUOTE_DOUBLE = "\"";
+
+    /**
+     * 逗号（半角）
+     */
+    String SYMBOL_COMMA_HALF = ",";
+
+    /**
+     * 逗号（全角）
+     */
+    String SYMBOL_COMMA_WHOLE = "，";
+
+    /**
+     * 句号（半角）
+     */
+    String SYMBOL_PERIOD_HALF = ".";
+
+    /**
+     * 句号（全角）
+     */
+    String SYMBOL_PERIOD_WHOLE = "。";
+
+    /**
+     * 回车
+     */
+    String SYMBOL_ENTER = "\n";
+
+    /**
+     * 换行
+     */
+    String SYMBOL_NEWLINE = "\r";
+
+}

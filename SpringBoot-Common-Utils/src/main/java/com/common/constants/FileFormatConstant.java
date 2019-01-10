135b0e69159742e263015fd421757fc3a08b3d90
wangqiang <qiang.wang@seedeer.com>
2019-01-10 15:24:53 +0800

    文件格式常量类

---
+++ SpringBoot-Common-Utils/src/main/java/com/common/constants/FileFormatConstant.java
@@ -0,0 +1,38 @@
+package com.common.constants;
+
+/**
+ * 文件格式常量类
+ *
+ * @author Dragon-zg
+ * @date 2018/12/1 15:27
+ **/
+public interface FileFormatConstant {
+    /**
+     * PDF
+     */
+    String PDF = "pdf";
+    /**
+     * PNG
+     */
+    String PNG = "png";
+    /**
+     * HTML
+     */
+    String HTML = "html";
+    /**
+     * RAR
+     */
+    String RAR = "rar";
+    /**
+     * ZIP
+     */
+    String ZIP = "zip";
+    /**
+     * DOC
+     */
+    String DOC = "doc";
+    /**
+     * TXT
+     */
+    String TXT = "txt";
+}

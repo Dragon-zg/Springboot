cf9263b75a63316e55fce0892b681fa18051081e
wangqiang <qiang.wang@seedeer.com>
2019-04-22 16:39:32 +0800

    使用 lombok

--- SpringBoot-Web-JPA/src/main/java/com/jpa/entity/Girl.java
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/entity/Girl.java
@@ -1,6 +1,7 @@
 package com.jpa.entity;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.persistence.*;
 
@@ -8,55 +9,23 @@ import javax.persistence.*;
  * @Description:
  * @Date: 2017-12-16 19:53
  */
+@Data
 @Entity
-@Table(name ="girl", schema = "test")
+@Table(name = "girl", schema = "test")
 public class Girl {
-    @ApiModelProperty("主键ID")
-    private Integer id;
-
-    @ApiModelProperty("")
-    private String cupSize;
-
-    @ApiModelProperty("年纪")
-    private Integer age;
-
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    @ApiModelProperty("主键ID")
+    private Integer id;
 
     @Basic
     @Column(name = "cup_size")
-    public String getCupSize() {
-        return cupSize;
-    }
-
-    public void setCupSize(String cupSize) {
-        this.cupSize = cupSize;
-    }
+    @ApiModelProperty("")
+    private String cupSize;
 
     @Basic
     @Column(name = "age")
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Girl{" +
-                "id=" + id +
-                ", cupSize='" + cupSize + '\'' +
-                ", age=" + age +
-                '}';
-    }
+    @ApiModelProperty("年纪")
+    private Integer age;
 }
--- SpringBoot-Web/src/main/java/com/web/config/InitRunner.java
+++ SpringBoot-Web/src/main/java/com/web/config/InitRunner.java
@@ -1,5 +1,6 @@
 package com.web.config;
 
+import com.common.enums.DateFormat;
 import com.common.utils.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +13,14 @@ import org.springframework.stereotype.Component;
  * @date 2019/1/10 15:04
  */
 @Component
-public class InitParamStartupRunner implements CommandLineRunner {
-    private final static Logger logger = LoggerFactory.getLogger(InitParamStartupRunner.class);
+public class InitRunner implements CommandLineRunner {
+    private final static Logger logger = LoggerFactory.getLogger(InitRunner.class);
 
 
     @Override
     public void run(String... args) throws Exception {
         //打印当前服务器时间
-        logger.info("当前项目启动时间是: {}", DateUtil.getNowDate());
+        logger.info("当前项目启动时间是: {}", DateUtil.getNowStr(DateFormat.CHINA_TIME));
     }
 
 }

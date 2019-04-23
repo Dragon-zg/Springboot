397454bdfd62eeeb317cfaaa7148c4bb867498b5
itter <534035217@qq.com>
2019-04-23 20:59:25 +0800

    bug

--- SpringBoot-Web-JPA/src/main/java/com/jpa/entity/manttomany/Student.java
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/entity/manttomany/Student.java
@@ -1,19 +1,15 @@
 package com.jpa.entity.manttomany;
 
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
 /**
  * 学生实体
  * @author wangqiang
  * @date 2019-03-13 22:59
  **/
-@ApiModel("学生实体")
-@Entity
-@Table(name = "jpa_student", schema = "test")
+//@ApiModel("学生实体")
+//@Entity
+//@Table(name = "jpa_student", schema = "test")
 public class Student {
     @ApiModelProperty("主键ID")
     private Integer id;

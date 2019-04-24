95bfeddea5656559e51d4bb16fd4e7dd8dd4481e
wangqiang <qiang.wang@seedeer.com>
2019-04-24 20:31:49 +0800

    初始化公民数据

--- SpringBoot-Web-JPA/src/main/java/com/jpa/controller/PersonController.java
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/controller/PersonController.java
@@ -2,7 +2,9 @@ package com.jpa.controller;
 
 import com.jpa.service.PersonService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +19,10 @@ public class PersonController {
 
     @Autowired
     private PersonService personService;
+
+    @ApiOperation(value = "初始化公民数据")
+    @GetMapping(value = {"/initPerson"})
+    public void initPerson() {
+        personService.initPerson();
+    }
 }

ed76c0733ef5b0a64d5cdb6e9400a3060694aef9
wangqiang <qiang.wang@seedeer.com>
2019-05-29 14:18:24 +0800

    Assert

--- SpringBoot-Web-JPA/src/main/java/com/jpa/service/base/AbstractCurdService.java
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/service/base/AbstractCurdService.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.Assert;
 
 import java.util.Collection;
 import java.util.List;
@@ -46,6 +47,7 @@ public abstract class AbstractCurdService<T extends BaseEntity, ID> implements C
      */
     @Override
     public List<T> listAll(Sort sort) {
+        Assert.notNull(sort, "sort is must not null.");
         return repository.findAll(sort);
     }
 
@@ -58,6 +60,8 @@ public abstract class AbstractCurdService<T extends BaseEntity, ID> implements C
      */
     @Override
     public List<T> listAll(Specification<T> spec, Sort sort) {
+        Assert.notNull(spec, "specification is must not null.");
+        Assert.notNull(sort, "sort is must not null.");
         return repository.findAll(spec, sort);
     }
 
@@ -70,6 +74,8 @@ public abstract class AbstractCurdService<T extends BaseEntity, ID> implements C
      */
     @Override
     public Page<T> pagingList(Map<String, Object> searchParam, Pageable pageable) {
+        Assert.notNull(searchParam, "searchParam is must not null.");
+        Assert.notNull(pageable, "pageable is must not null.");
         Collection<SearchFilter> filters = SearchFilter.parse(searchParam);
         Specification<T> specification = DynamicSpecification.bySearchFilter(filters);
         Page<T> page = repository.findAll(specification, pageable);

5c375c2b9188110573f23c61704cc3d463c086d8
wangqiang <qiang.wang@seedeer.com>
2019-06-06 15:25:49 +0800

    代码调整

--- SpringBoot-Webservice/src/main/java/com/cxf/server/config/CxfConfig.java
+++ SpringBoot-Webservice/src/main/java/com/cxf/server/config/CxfConfig.java
@@ -7,30 +7,34 @@ import com.cxf.server.service.UserService;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.apache.cxf.transport.servlet.CXFServlet;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import javax.xml.ws.Endpoint;
 
 /**
  * CXF 配置类
- * @Date: 2018-06-03 14:45
- * @Author: Dragon-zg
+ * @date: 2018-06-03 14:45
+ * @author: Dragon-zg
  */
-@Component
+@Configuration
+@SuppressWarnings("unchecked")
 public class CxfConfig {
-    @Autowired
-    private Bus bus;
 
-    @Autowired
-    private UserService userService;
+    private final Bus bus;
 
-    /** 
-     * @Description 设置cxfServlet根路径
-     * @Author Dragon-zg 
-     * @Date 2018/6/3 15:59
+    private final UserService userService;
+
+    public CxfConfig(Bus bus, UserService userService) {
+        this.bus = bus;
+        this.userService = userService;
+    }
+
+    /**
+     * @description 设置cxfServlet根路径
+     * @author Dragon-zg
+     * @date 2018/6/3 15:59
      * @return org.springframework.boot.web.servlet.ServletRegistrationBean
      */
     @Bean(name = "cxfServlet")

dcf488730cb36afa2ae4b970cbbabf91b3351546
wangqiang <qiang.wang@seedeer.com>
2019-06-11 15:19:47 +0800

    @ManyToMany

---
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/model/dto/TeacherDTO.java
@@ -0,0 +1,16 @@
+package com.jpa.model.dto;
+
+import com.jpa.model.converter.InputConverter;
+import com.jpa.model.entity.unidirectional.manttomany.Teacher;
+import lombok.Data;
+
+/**
+ * Person DTO
+ * @author Dragon-zg
+ * @date 2019/6/10 13:40
+ **/
+@Data
+public class TeacherDTO implements InputConverter<Teacher> {
+
+    private String name;
+}
---
+++ SpringBoot-Web-JPA/src/main/java/com/jpa/repository/TeacherRepository.java
@@ -0,0 +1,13 @@
+package com.jpa.repository;
+
+import com.jpa.model.entity.unidirectional.manttomany.Teacher;
+import com.jpa.repository.base.BaseRepository;
+
+/**
+ * TeacherRepository
+ *
+ * @author Dragon-zg
+ * @date 2019/6/11 14:29
+ */
+public interface TeacherRepository extends BaseRepository<Teacher, Long> {
+}

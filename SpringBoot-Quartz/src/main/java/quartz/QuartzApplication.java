b88edbfb2b3b62d9e71d3c43952472e47477ce6e
wangqiang <qiang.wang@seedeer.com>
2019-06-12 11:57:28 +0800

    SpringBoot-Quartz module

---
+++ SpringBoot-Quartz/src/main/java/quartz/QuartzApplication.java
@@ -0,0 +1,16 @@
+package quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dragon-zg
+ * @date 2018/8/28 19:44
+ */
+@SpringBootApplication
+public class QuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzApplication.class, args);
+    }
+}

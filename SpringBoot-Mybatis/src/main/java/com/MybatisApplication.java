4c53fc755dc42c512ed4ab830a39186a27a4fe6c
wangqiang <qiang.wang@seedeer.com>
2019-06-13 16:56:23 +0800

    SpringBoot-Mybatis module

---
+++ SpringBoot-Mybatis/src/main/java/com/MybatisApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * MybatisApplication
+ *
+ * @author Dragon-zg
+ * @date 2019/6/13 16:52
+ */
+@SpringBootApplication
+@PropertySource("classpath:env/${spring.profiles.active}/application.properties")
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}

e71469c4ce1c06f97841481619d0d0baee02550c
wangqiang <qiang.wang@seedeer.com>
2019-06-17 15:54:58 +0800

    UserMapper

--- SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/mapper/UserMapper.java
+++ SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/mapper/UserMapper.java
@@ -2,7 +2,6 @@ package com.lnnk.mybatis.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lnnk.mybatis.model.entity.User;
-import org.springframework.stereotype.Repository;
 
 /**
  * UserMapper
@@ -10,6 +9,5 @@ import org.springframework.stereotype.Repository;
  * @author Lnnk
  * @date 2019/6/14 14:28
  **/
-@Repository
 public interface UserMapper extends BaseMapper<User> {
 }

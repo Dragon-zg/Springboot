3e11180d8512251be3761acc095f017b077f60ac
itter <534035217@qq.com>
2019-06-17 22:38:41 +0800

    DataSource

--- SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/config/MybatisPlusConfig.java
+++ SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/config/MybatisPlusConfig.java
@@ -2,8 +2,7 @@ package com.lnnk.mybatis.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,16 +25,9 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
-    @Autowired
-    private DataSourceProperties dataSourceProperties;
-
-    @Bean(name = "dataSource")
-    public DataSource dataSource() {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(dataSourceProperties.getUrl());
-        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
-        dataSource.setUsername(dataSourceProperties.getUsername());
-        dataSource.setPassword(dataSourceProperties.getPassword());
-        return dataSource;
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource getDataSource(){
+        return new DruidDataSource();
     }
 }

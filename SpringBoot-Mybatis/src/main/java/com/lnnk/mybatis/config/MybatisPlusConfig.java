db628e04bd52cc2c8cf7d7965ab9c64c7490473c
wangqiang <qiang.wang@seedeer.com>
2019-06-18 10:08:40 +0800

    spring aop

--- SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/config/MybatisPlusConfig.java
+++ SpringBoot-Mybatis/src/main/java/com/lnnk/mybatis/config/MybatisPlusConfig.java
@@ -1,12 +1,11 @@
 package com.lnnk.mybatis.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import com.lnnk.web.constant.EnvConsts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
+import org.springframework.context.annotation.Profile;
 
 /**
  * MybatisPlus配置类
@@ -25,9 +24,12 @@ public class MybatisPlusConfig {
         return new PaginationInterceptor();
     }
 
+    /**
+     * SQL执行效率插件,测试环境
+     */
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource getDataSource(){
-        return new DruidDataSource();
+    @Profile({EnvConsts.DEVELOPMENT})
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }

c4114e167f7816506c8c5cdc29b6a967a60755fd
wangqiang <qiang.wang@seedeer.com>
2019-06-19 17:27:04 +0800

    insert

--- SpringBoot-Mybatis/src/test/java/com/lnnk/mybatis/MybatisApplicationTest.java
+++ SpringBoot-Mybatis/src/test/java/com/lnnk/mybatis/MybatisApplicationTest.java
@@ -1,7 +1,7 @@
 package com.lnnk.mybatis;
 
-import com.lnnk.mybatis.mapper.UserMapper;
 import com.lnnk.mybatis.model.entity.User;
+import com.lnnk.mybatis.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 /**
  * @author wangqiang
@@ -19,7 +20,7 @@ import java.time.LocalDateTime;
 public class MybatisApplicationTest {
 
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
     @Test
     public void insertTest() {
@@ -28,8 +29,27 @@ public class MybatisApplicationTest {
         user.setAge(10);
         user.setEmail("first@baomidou.com");
         user.setCreateTime(LocalDateTime.now());
-        int rows = userMapper.insert(user);
-        System.out.println("影响记录数：" + rows);
+        boolean flag = userService.save(user);
+        System.out.println("是否成功：" + flag);
         System.out.println("主键：" + user.getId());
+    }
+
+    @Test
+    public void batchInsertTest() {
+        User first = new User();
+        first.setName("first");
+        first.setAge(10);
+        first.setEmail("first@baomidou.com");
+        first.setCreateTime(LocalDateTime.now());
+
+        User second = new User();
+        second.setName("second");
+        second.setAge(20);
+        second.setEmail("second@baomidou.com");
+        second.setCreateTime(LocalDateTime.now());
+        boolean flag = userService.saveBatch(Arrays.asList(first, second));
+        System.out.println("是否成功：" + flag);
+        System.out.println("first主键：" + first.getId());
+        System.out.println("second主键：" + second.getId());
     }
 }

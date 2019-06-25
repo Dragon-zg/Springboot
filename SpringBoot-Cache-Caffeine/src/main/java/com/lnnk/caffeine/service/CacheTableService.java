8194de4cc3dad34251d7f3fa5927437a20a95bd1
wangqiang <qiang.wang@seedeer.com>
2019-06-25 16:53:12 +0800

    Caffeine配置说明

--- SpringBoot-Cache-Caffeine/src/main/java/com/lnnk/caffeine/service/CacheTableService.java
+++ SpringBoot-Cache-Caffeine/src/main/java/com/lnnk/caffeine/service/CacheTableService.java
@@ -1,5 +1,7 @@
 package com.lnnk.caffeine.service;
 
+import com.lnnk.caffeine.entity.CacheTable;
+
 /**
  * CacheTableService
  *
@@ -7,4 +9,28 @@ package com.lnnk.caffeine.service;
  * @date 2019/6/25 15:59
  **/
 public interface CacheTableService {
+
+    /**
+     * 保存并添加缓存
+     *
+     * @param cacheTable cacheTable
+     * @return com.lnnk.caffeine.entity.CacheTable
+     */
+    public CacheTable save(CacheTable cacheTable);
+
+    /**
+     * 删除并删除缓存
+     *
+     * @param id ID
+     * @return void
+     */
+    public void remove(Long id);
+
+    /**
+     * 查询并缓存
+     *
+     * @param id ID
+     * @return com.lnnk.caffeine.entity.CacheTable
+     */
+    public CacheTable findOne(Long id);
 }
--- SpringBoot-Cache-Caffeine/src/main/java/com/lnnk/caffeine/service/impl/CacheTableServiceImpl.java
+++ SpringBoot-Cache-Caffeine/src/main/java/com/lnnk/caffeine/service/impl/CacheTableServiceImpl.java
@@ -1,8 +1,17 @@
 package com.lnnk.caffeine.service.impl;
 
+import com.lnnk.caffeine.entity.CacheTable;
 import com.lnnk.caffeine.repository.CacheTableRepository;
 import com.lnnk.caffeine.service.CacheTableService;
+import com.lnnk.web.enums.ExceptionCode;
+import com.lnnk.web.exception.CustomizedException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * CacheTableServiceImpl
@@ -10,6 +19,7 @@ import org.springframework.stereotype.Service;
  * @author wangqiang
  * @date 2019/6/25 16:00
  **/
+@Log4j2
 @Service
 public class CacheTableServiceImpl implements CacheTableService {
 
@@ -19,4 +29,27 @@ public class CacheTableServiceImpl implements CacheTableService {
         this.cacheTableRepository = cacheTableRepository;
     }
 
+    @Override
+    @CachePut(value = "cacheTable", key = "#cacheTable.id")
+    public CacheTable save(CacheTable cacheTable) {
+        CacheTable saveModel = cacheTableRepository.save(cacheTable);
+        log.info("保存并新增缓存: {}", saveModel.toString());
+        return saveModel;
+    }
+
+    @Override
+    @CacheEvict(value = "cacheTable", key = "#id")
+    public void remove(Long id) {
+        cacheTableRepository.deleteById(id);
+        log.info("删除并删除缓存: {}", id);
+    }
+
+    @Override
+    @Cacheable(value = "people", key = "#id", sync = true)
+    public CacheTable findOne(Long id) {
+        Optional<CacheTable> optional = cacheTableRepository.findById(id);
+        CacheTable cacheTable = optional.orElseThrow(() -> new CustomizedException(ExceptionCode.DATA_NOT_EXIST));
+        log.info("查询并缓存: {}", cacheTable.toString());
+        return cacheTable;
+    }
 }

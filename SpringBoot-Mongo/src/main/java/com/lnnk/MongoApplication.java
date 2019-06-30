dc5ddd0179efd373e58dea2bbfbab01944b826ea
itter <534035217@qq.com>
2019-06-30 14:25:46 +0800

    @EnableMongoRepositories

--- SpringBoot-Mongo/src/main/java/com/lnnk/MongoApplication.java
+++ SpringBoot-Mongo/src/main/java/com/lnnk/MongoApplication.java
@@ -2,12 +2,14 @@ package com.lnnk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
  * @author Lnnk
  * @date 2019/6/12 16:01
  */
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.lnnk.mongo.repository")
 public class MongoApplication {
 
     public static void main(String[] args) {

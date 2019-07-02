f4d7d187924289371538248792a48bef5acc1113
itter <534035217@qq.com>
2019-07-02 21:26:24 +0800

    AOP日志切面编程 DEMO

--- SpringBoot-Example/src/main/java/com/lnnk/example/aspect/AspectLog.java
+++ SpringBoot-Example/src/main/java/com/lnnk/example/aspect/AspectLog.java
@@ -1,5 +1,7 @@
 package com.lnnk.example.aspect;
 
+import cn.hutool.extra.servlet.ServletUtil;
+import com.lnnk.web.constant.SymbolConsts;
 import com.lnnk.web.util.JsonUtils;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
@@ -23,48 +25,53 @@ import java.util.Arrays;
 @Log4j2
 public class AspectLog {
 
+    /**
+     * 切点
+     */
     @Pointcut("execution(public * com.lnnk.example.controller.TestController.aspect(..))")
-    public void index_log() {
+    public void indexLog() {
     }
 
     /**
      * 记录HTTP请求结束时的日志
      */
-    @Before("index_log()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
+    @Before("indexLog()")
+    public void doBefore(JoinPoint joinPoint) {
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
-        // 记录下请求内容
-        log.info(">>>>>>>>>>Before");
-        log.info("URL : " + request.getRequestURL().toString());
-        log.info("HTTP_METHOD : " + request.getMethod());
-        log.info("IP : " + request.getRemoteAddr());
-        log.info("PATH : " + request.getServletPath());
-        log.info("METHOD : " + request.getMethod());
-        log.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        log.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
+        if (null != attributes) {
+            HttpServletRequest request = attributes.getRequest();
+            log.info("Before >>>>>>>>>> URL: {}, HTTP_METHOD: {}, IP: {}, PATH: {}, METHOD: {}, CLASS_METHOD: {}, ARGS: {}",
+                    request.getRequestURL().toString(),
+                    request.getMethod(),
+                    ServletUtil.getClientIP(request),
+                    request.getServletPath(),
+                    request.getMethod(),
+                    joinPoint.getSignature().getDeclaringTypeName() + SymbolConsts.SYMBOL_PERIOD_HALF + joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
     }
 
-    @AfterReturning(returning = "obj", pointcut = "index_log()")
-    public void doAfterReturning(Object obj) throws Throwable {
+    @AfterReturning(returning = "obj", pointcut = "indexLog()")
+    public void doAfterReturning(Object obj) {
         //处理完请求，返回内容
         log.info("AfterReturning >>>>>>>>>> RESPONSE: {}", JsonUtils.toJson(obj));
     }
 
-    @AfterThrowing(value = "index_log()", throwing = "exception")
+    @AfterThrowing(value = "indexLog()", throwing = "exception")
     public void doAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        //目标方法名：
-        log.info(">>>>>>>>>>AfterThrowing");
-        log.info(joinPoint.getSignature().getName());
+        String exMsg;
         if (exception instanceof NullPointerException) {
-            log.info("发生了空指针异常!!!!!");
+            exMsg = "发生了空指针异常!";
         } else {
-            log.info("发生了未知异常!!!!!");
+            exMsg = "发生了未知异常!";
         }
+        log.info("AfterThrowing >>>>>>>>>> CLASS_METHOD: {}, exMsg: {}",
+                joinPoint.getSignature().getName(), exMsg);
     }
 
-    @Around(value = "index_log()")
+    @Around(value = "indexLog()")
     public Object doAroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
         long startTime = System.currentTimeMillis();
         String name = "-";
@@ -77,7 +84,8 @@ public class AspectLog {
             throw throwable;
         } finally {
             long endTime = System.currentTimeMillis();
-            log.info("Around >>>>>>>>>> 方法名: {} 是否成功: {} 执行时间: {}ms", name, result, endTime - startTime);
+            log.info("Around >>>>>>>>>> 方法名: {}, 是否成功: {}, 执行时间: {}ms",
+                    name, result, endTime - startTime);
         }
     }
 }
--- SpringBoot-Example/src/main/java/com/lnnk/example/controller/TestController.java
+++ SpringBoot-Example/src/main/java/com/lnnk/example/controller/TestController.java
@@ -2,10 +2,14 @@ package com.lnnk.example.controller;
 
 import com.lnnk.example.annotation.IpStint;
 import com.lnnk.example.i18n.I18nUtil;
+import com.lnnk.web.enums.ExceptionCode;
+import com.lnnk.web.exception.CustomizedException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -33,6 +37,11 @@ public class TestController {
 
     @ApiOperation("切面Demo测试")
     @GetMapping("/aspect")
-    public void aspect() {
+    public void aspect(@RequestParam(value = "type", required = false) String type) {
+        if (StringUtils.equals("1", type)) {
+            throw new NullPointerException();
+        } else if (StringUtils.equals("2", type)) {
+            throw new CustomizedException(ExceptionCode.PARAM_ERROR);
+        }
     }
 }

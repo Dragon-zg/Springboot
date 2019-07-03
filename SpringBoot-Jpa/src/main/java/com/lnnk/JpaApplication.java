f2d8c83076b532640eed5ba5143cddb62759e78a
itter <534035217@qq.com>
2019-07-03 21:39:51 +0800

    Hibernate5Module

--- SpringBoot-Jpa/src/main/java/com/lnnk/JpaApplication.java
+++ SpringBoot-Jpa/src/main/java/com/lnnk/JpaApplication.java
@@ -1,10 +1,12 @@
 package com.lnnk;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.lnnk.jpa.repository.base.impl.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
@@ -25,5 +27,10 @@ public class JpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaApplication.class, args);
+    }
+
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        return new Hibernate5Module();
     }
 }

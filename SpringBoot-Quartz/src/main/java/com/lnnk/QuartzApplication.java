74401a13e4cfb6dd6bc3f199e523a36a9b6a007a
wangqiang <qiang.wang@seedeer.com>
2019-07-17 17:20:09 +0800

    SpringBoot-Quartz init

--- SpringBoot-Quartz/src/main/java/com/lnnk/QuartzApplication.java
+++ SpringBoot-Quartz/src/main/java/com/lnnk/QuartzApplication.java
@@ -1,13 +1,15 @@
-package com;
+package com.lnnk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Lnnk
  * @date 2019/6/12 16:01
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class QuartzApplication {
 
     public static void main(String[] args) {
---
+++ SpringBoot-Quartz/src/main/java/com/lnnk/quartz/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.lnnk.quartz.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置类
+ *
+ * @author Lnnk
+ * @date 2019-06-16 20:28
+ **/
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件物理分页
+     */
+    @Bean
+    public PaginationInterceptor getPaginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}

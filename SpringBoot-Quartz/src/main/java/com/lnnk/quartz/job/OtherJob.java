8bb1c5c1a235e8190cd94b5c01df963f5b5f0545
wangqiang <qiang.wang@seedeer.com>
2019-08-07 11:50:20 +0800

    @DisallowConcurrentExecution

--- SpringBoot-Quartz/src/main/java/com/lnnk/quartz/job/OtherJob.java
+++ SpringBoot-Quartz/src/main/java/com/lnnk/quartz/job/OtherJob.java
@@ -1,6 +1,7 @@
 package com.lnnk.quartz.job;
 
 import lombok.extern.log4j.Log4j2;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
@@ -10,10 +11,18 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
  * @date 2019/7/17 17:41
  **/
 @Log4j2
+@DisallowConcurrentExecution
 public class OtherJob extends QuartzJobBean {
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        log.info("OtherJob!");
+        log.info("OtherJob! @DisallowConcurrentExecution注解后不会并发执行, 当前执行完成后才会执行下次的定时任务!");
+        try {
+            Thread.sleep(15000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            log.info("OtherJob END!");
+        }
     }
 }
